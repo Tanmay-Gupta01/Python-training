b8cb9d74ebeb4fb43aa660f911f92881ea0ebd75
Tanmay_Gupta <tanmaygupta739@gmail.com>
2025-04-15 22:54:59 +0530

    beginner level done

--- day10/First_task/question1.py
+++ day10/First_task/question1.py
@@ -3,7 +3,7 @@ import csv
 scores = []
 avg =0
 
-with open('day10\\students.txt', 'r') as file:
+with open('day10\\First_task\\question1.py', 'r') as file:
     for line in file:
         name, score = line.strip().split(',')
         scores.append([name, int(score)])
@@ -14,13 +14,13 @@ for i in scores:
 average = avg/len(scores)
 
 
-with open('day10\\top_students.txt','w') as file2:
+with open('day10\\First_task\\top_students.txt','w') as file2:
     for name,score in scores:
         if average<score :
             file2.write(f"{name} : {score}\n")
     # print(file2.read)
 
-with open('day10\\top_student.csv','a', newline='') as csvfile :
+with open('day10\\First_task\\top_student.csv','a', newline='') as csvfile :
     writer = csv.DictWriter(csvfile, fieldnames=['Name', 'Score'])
     if csvfile.tell() == 0:  # Write header only if file is empty
             writer.writeheader()
@@ -28,7 +28,7 @@ with open('day10\\top_student.csv','a', newline='') as csvfile :
                  if average<score :
                      writer.writerow({'Name': name, 'Score': score})
 
-with open('day10\\top_student.csv', 'r') as file:
+with open('day10\\First_task\\top_student.csv', 'r') as file:
             reader = csv.DictReader(file)
             for row in reader:
                 print(row)
---
+++ day10/Second_task/beginner.py
@@ -0,0 +1,19 @@
+# # 1 st question 
+# with open('day10\\Second_task\\notes.txt','r') as file:
+#     notes = file.readlines()
+#     print(notes)
+
+
+# # 2 nd question
+# with open('day10\\Second_task\\notes.txt','r') as file:
+#     lines = len(file.readlines())
+#     print(lines)
+
+# # 3 rd question
+# with open('day10\\Second_task\\reminder.txt','w') as file:
+#     for i in range(1,6):
+#         file.write(f"task {i}\n")
+
+# # 4 th question
+# with open('day10\\Second_task\\reminder.txt','a') as file:
+#     file.write("task 6\n")
