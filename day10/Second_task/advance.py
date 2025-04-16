03482238cfdaf4cfb4f6061d9e854a1a56b9cc61
Tanmay_Gupta <tanmaygupta739@gmail.com>
2025-04-16 15:03:16 +0530

    14th question solved

--- day10/Second_task/advance.py
+++ day10/Second_task/advance.py
@@ -1,9 +1,9 @@
 # #  11th question
 
-# with open('day10\\Second_task\\server.log','r')as file:
+# with open('day10\\\Second_task\\\server.log','r')as file:
 #     lines = file.readlines()
 # count = 0
-# with open('day10\\Second_task\\errors_only.log','w')as fp:
+# with open('day10\\\Second_task\\\errors_only.log','w')as fp:
 #     for line in lines:
 #         if "ERROR" in line:
 #             fp.write(line)
@@ -15,20 +15,42 @@
 # # 12 th question
 
 # from collections import Counter
-# with open('day10\\Second_task\\input.txt','r') as file:
+# with open('day10\\\Second_task\\\input.txt','r') as file:
 #     lines = file.read().lower()
 
 # words = lines.split()
 # dictionary = Counter(words)
-# with open('day10\\Second_task\\frequency.txt','w') as fp:
+# with open('day10\\\Second_task\\\frequency.txt','w') as fp:
 #     for word, frequency in dictionary.items():
 #         fp.write(f'{word} : {frequency}\n')
 
 
 
-# 13 th question
+# # 13 th question
 
-with open('day10\\Second_task\\sales.csv','r')as file:
-    line = file.read()
-    lines = line.split('\n')
-    
+# with open('day10\\\Second_task\\\sales.csv', 'r') as file, open('day10\\\Second_task\\\high_sales.csv', 'w') as fp:
+#     lines = file.readlines()
+#     fp.write(lines[0])  
+
+#     for line in lines[1:]:
+#         date, customer, sales = line.strip().split(',')
+#         if int(sales) > 10000:
+#             fp.write(f'{date},{customer},{sales}\n')
+
+
+
+# 14 th question
+
+with open('day10\\Second_task\\input.txt','r') as file1, open('day10\\Second_task\\output.txt', 'r') as file2 ,open('day10\\Second_task\\frequency.txt','r') as file3, open('day10\\Second_task\\full_book.txt','w')as f:
+    lines1 = file1.readlines()
+    lines2 = file2.readlines()
+    lines3 = file3.readlines()
+    f.write('--- From input.txt ---\n')
+    f.writelines(lines1)
+    f.write('\n--- From output.txt ---\n')
+    f.writelines(lines2)
+    f.write('\n--- From frequency.txt ---\n')
+    f.writelines(lines3)
+
+
+# 15 th question
