2144fa0fbd10268cce7b602368ab9f309f2b869c
Tanmay_Gupta <tanmaygupta739@gmail.com>
2025-04-16 15:14:23 +0530

    advance level done

--- day10/Second_task/advance.py
+++ day10/Second_task/advance.py
@@ -1,41 +1,41 @@
-# #  11th question
+#  11th question
 
-# with open('day10\\\Second_task\\\server.log','r')as file:
-#     lines = file.readlines()
-# count = 0
-# with open('day10\\\Second_task\\\errors_only.log','w')as fp:
-#     for line in lines:
-#         if "ERROR" in line:
-#             fp.write(line)
-#             count += 1
-#     print(count)
-
-
-
-# # 12 th question
-
-# from collections import Counter
-# with open('day10\\\Second_task\\\input.txt','r') as file:
-#     lines = file.read().lower()
-
-# words = lines.split()
-# dictionary = Counter(words)
-# with open('day10\\\Second_task\\\frequency.txt','w') as fp:
-#     for word, frequency in dictionary.items():
-#         fp.write(f'{word} : {frequency}\n')
+with open('day10\\\Second_task\\\server.log','r')as file:
+    lines = file.readlines()
+count = 0
+with open('day10\\\Second_task\\\errors_only.log','w')as fp:
+    for line in lines:
+        if "ERROR" in line:
+            fp.write(line)
+            count += 1
+    print(count)
 
 
 
-# # 13 th question
+# 12 th question
 
-# with open('day10\\\Second_task\\\sales.csv', 'r') as file, open('day10\\\Second_task\\\high_sales.csv', 'w') as fp:
-#     lines = file.readlines()
-#     fp.write(lines[0])  
+from collections import Counter
+with open('day10\\\Second_task\\\input.txt','r') as file:
+    lines = file.read().lower()
 
-#     for line in lines[1:]:
-#         date, customer, sales = line.strip().split(',')
-#         if int(sales) > 10000:
-#             fp.write(f'{date},{customer},{sales}\n')
+words = lines.split()
+dictionary = Counter(words)
+with open('day10\\\Second_task\\\frequency.txt','w') as fp:
+    for word, frequency in dictionary.items():
+        fp.write(f'{word} : {frequency}\n')
+
+
+
+# 13 th question
+
+with open('day10\\\Second_task\\\sales.csv', 'r') as file, open('day10\\\Second_task\\\high_sales.csv', 'w') as fp:
+    lines = file.readlines()
+    fp.write(lines[0])  
+
+    for line in lines[1:]:
+        date, customer, sales = line.strip().split(',')
+        if int(sales) > 10000:
+            fp.write(f'{date},{customer},{sales}\n')
 
 
 
@@ -54,3 +54,10 @@ with open('day10\\Second_task\\input.txt','r') as file1, open('day10\\Second_tas
 
 
 # 15 th question
+
+import os 
+path = "day10\Second_task"
+all = os.listdir(path) 
+files = [file for file in all if file.endswith('.txt') or file.endswith('.csv')]
+print("txt and csv Files in '", path, "' :") 
+print(files) 
